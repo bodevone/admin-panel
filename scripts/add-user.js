b24f90a405ceebbc7f2e13b076d7f838fbd45c69
bodevone <aliby.bbb@gmail.com>
2019-01-14 13:18:01 +0600

    User can be deleted as well as driver Changes to consider: - Delete User of driver when Driver is deleted - Delete Driver Location and Driver Pins Data

--- scripts/add-user.js
+++ scripts/add-user.js
@@ -10,7 +10,7 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
-function addDriver() {
+function addUser() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     console.log(username);
@@ -55,4 +55,87 @@ function addUserToDatabase(uid, user, psw) {
     refUsers.child(uid).update({driver: driverId});
 
     location.reload();
+}
+
+
+//Deleting user
+function openDelete() {
+    document.getElementById("deleteForm").style.display = "block";
+}
+
+function closeDelete() {
+    document.getElementById("deleteForm").style.display = "none";
+}
+
+function deleteUser () {
+    var username = document.getElementById("nameForDelete").value;
+    console.log(username);
+
+    findUserId(username);
+}
+
+function findUserId(username) {
+
+    var query = refAccounts.orderByKey();
+    query.once("value").then(function(snapshot) {
+        var foundUser = false;
+        var userId;
+        var name;
+        var pass;
+        snapshot.forEach(function(childSnapshot) {
+            var user = childSnapshot.child('username').val();
+            if (username == user) {
+                foundUser = true;
+                userId = childSnapshot.key;
+                name =  user;
+                pass =  childSnapshot.child('password').val();
+            }
+            // Cancel enumeration
+        });
+        if (foundUser) {
+            console.log(userId);
+            deleteFromAuth(userId, name, pass);
+        } else {
+            alert('Имя Пользователя Было Введено не Правильно');
+        }
+    });
+}
+
+function deleteFromAuth(userId, username, password) {
+    console.log(username);
+    console.log(password);
+    firebase.auth().signInWithEmailAndPassword(username, password).then(function() {
+        var user = firebase.auth().currentUser;
+
+        //Sign-out
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+            console.log('Sign out successful');
+
+            user.delete().then(function() {
+                console.log('User Deleted');
+                deleteFromDatabase(userId)
+            }).catch(function(error) {
+                // An error happened.
+                console.log('Error');
+            });
+        }).catch(function(error) {
+            // An error happened.
+        });
+
+    }).catch(function(error) {
+        console.log('Error');
+    });
+}
+
+function deleteFromDatabase(userId) {
+    refAccounts.child(userId).remove().then(function() {
+        refUsers.child(userId).remove().then(function() {
+            location.reload();
+        }).catch(function(error) {
+            console.log('Error');
+        });
+    }).catch(function(error) {
+        console.log('Error');
+    });
 }
