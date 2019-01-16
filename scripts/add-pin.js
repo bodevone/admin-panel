67dc850e2db89fd45d9c9b870aa555e1198bd803
yerassylsagynov <yerassyl.sagynov@nu.edu.kz>
2019-01-16 17:57:58 +0600

    Update draggable markers location

--- scripts/add-pin.js
+++ scripts/add-pin.js
@@ -30,34 +30,56 @@ function idToUsername(driversId) {
 
 var driversPins = new Array();
 for (var i = 0; i < 100; i++) {
-    var marker = new mapboxgl.Marker();
+    var marker = new mapboxgl.Marker({
+        draggable: true
+    });
     driversPins.push(marker);
 }
 
 
 function showDriverPins(driversId) {
     var refPins = database.ref('driver-pins');
-    for(var i = 0; i < 100; i++) {
-        driversPins[i].remove();
-    }
+    // var pin_it = 0;
+    // for(var i = 0; i < 100; i++) {
+    //     driversPins[i].remove();
+    // }
     refPins.on('value', gotDataPins, errDataPins);
     function gotDataPins(data) {
         for (var key in data.val()) {
             if (data.val().hasOwnProperty(key) && key == driversId) {
                 var pinsArray = data.val()[key];
                 var pin_it = 0;
+                for(var i = 0; i < 100; i++) {
+                    driversPins[i].remove();
+                }
                 for(var k in pinsArray) {
                     if(pinsArray.hasOwnProperty(k)) {
-                        var popup = new mapboxgl.Popup({ offset: 25 })
+                        var popup = new mapboxgl.Popup({ offset: 25, className: k })
                             .setText(k);
                         var pinLon = pinsArray[k].longitude;
                         var pinLat = pinsArray[k].latitude;
-                        driversPins[pin_it++] = new mapboxgl.Marker().setLngLat([pinLon, pinLat]).setPopup(popup).addTo(map);
+                        driversPins[pin_it++] = new mapboxgl.Marker({draggable : true}).setLngLat([pinLon, pinLat]).setPopup(popup).addTo(map);
+                        console.log(pin_it);
                     }
                 }
+
+                for(var i = 0; i < pin_it; i++) {
+                    driversPins[i].on('dragend', onDragEnd);
+                } 
             }
         }
     }
+
+    function onDragEnd(e) {
+        var pinName = e.target._popup['options']['className'];
+        var pinLng = e.target._lngLat['lng'];
+        var pinLat = e.target._lngLat['lat']
+        firebase.database().ref('driver-pins/' + driversId + "/" + pinName).set({
+            latitude: pinLat,
+            longitude: pinLng
+        });
+    }
+
     function errDataPins(err) {
         console.log('Error');
         console.log(err);
@@ -66,7 +88,9 @@ function showDriverPins(driversId) {
 
 //Function to push data to table
 function dataToTable(driversToTable, driversId) {
-    var str1 = "Era pidr))"
-    var str = "<tr><td><button class=\"btn btn-primary\" onclick=showDriverPins(\""+ driversId + "\")>" + driversToTable + "</button></tr></td>";
-    $("#addPinTable").append(str);
+    var str0 = "<tr><td><button class=\"btn btn-primary\" onclick=showDriverPins(\""+ driversId + "\")>" + driversToTable + "</button>";
+    var str1 = "<button id=\"addDriverPinButton\" type=\"button\" class=\"btn btn-success\">+</button>";
+    var str2 = "<button id=\"removeDriverPinButton\" type=\"button\" class=\"btn btn-danger\">-</button>";
+    var end = "</tr></td>";
+    $("#addPinTable").append(str0 + str1 + str2 + end);
 }
